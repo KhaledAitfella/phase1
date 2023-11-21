17fa79b5d5e0a543a343fa235b6da046d296c90d
khaled Ait Fella <khait3@ulaval.ca>
2023-11-20 21:55:22 -0500

    correction erreur3

--- phase1.py
+++ phase1.py
@@ -1,13 +1,21 @@
+'''
+le programme  fait l'analyse de la ligne de la commande et recupere les information
+pour ensuite faire une recherche dans un site de titres bourssiers
+et recupere toute l'activite du symbole donne dans une periode de temps donne
+et recuperer la valeur desire et l'afficher d'une maniere specifier dans le terminal
+'''
 import argparse
 import datetime
 import json
 import requests
 
 def analyse_commande():
-    """
-    Générer un interpréteur de commande et retourne un  renvoie un objet Namespace avec l'attribut «symboles» pour la liste des symboles
-    et les attributs «début», «fin» et «valeur» pour les arguments optionnels de la ligne de commande
-    """
+    '''
+    Générer un interpréteur de commande et retourne un  renvoie un
+    objet Namespace avec l'attribut «symboles» pour la liste des
+    symboles et les attributs «début», «fin» et «valeur» pour
+    les arguments optionnels de la ligne de commande
+    '''
     parser = argparse.ArgumentParser(description="Extraction de valeurs historiques pour un"
                                      "ou plusieurs symboles boursiers.")
     parser.add_argument(
@@ -42,7 +50,7 @@ def analyse_commande():
     return parser.parse_args()
 
 def historique_serveur(symbole__, debut, fin):
-    ##fait la recherche dans un site url d'un symbole donner en entree
+    '''fait la recherche dans un site url d'un symbole donner en entree'''
     url = f'https://pax.ulaval.ca/action/{symbole__}/historique/'
 
     params = {
@@ -53,12 +61,12 @@ def historique_serveur(symbole__, debut, fin):
     return json.loads(réponse.text)
 
 def produire_historique(symbole_, debut, fin, valeur):
-    #affiche les tuples [date, valeur] retourner par l'appelle de la fonction istorique_serveur
+    '''affiche les tuples [date, valeur] retourner par l'appelle de la fonction istorique_serveur'''
     historique = historique_serveur(symbole_, debut, fin)
     if historique is not None:
         for date, valeurs in historique.get('historique', {}).items():
             print(f'Date: {date}, Valeur ({valeur}): {valeurs[valeur]}')
-   
+
 args = analyse_commande()
 print(f'titre = {args.symbole[0]}: '
       f'valeur = {args.valeur}, '
@@ -67,4 +75,3 @@ print(f'titre = {args.symbole[0]}: '
 
 for symbole in args.symbole:
     produire_historique(symbole, args.date_deb, args.date_fin, args.valeur)
-
